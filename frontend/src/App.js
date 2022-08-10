6538bd2de183fe0a1579ff79f9e7d566edcbfb07
mikechirkov44 <ya.mikechirkov@yandex.ru>
2022-08-10 20:10:56 +0300

    front-end init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UserList from './components/User';
+import MainMenu from './components/header';
+import Footer from './components/footer';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const UserApi = 'http://127.0.0.1:8000/userapp/api/users/'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(UserApi)
+      .then(response => {
+        const users = response.data
+        this.setState(
+          {
+            'users': users
+          }
+        )
+      }).catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <>
+        <MainMenu />
+        <div>
+          <UserList users={this.state.users} />
+        </div >
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
