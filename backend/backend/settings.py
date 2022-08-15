199a5e7b78ded2658601949ad8b55b5d82e9846f
mikechirkov44 <ya.mikechirkov@yandex.ru>
2022-08-15 17:26:02 +0300

    settings modified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,16 +135,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'userapp.User'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     # temporary general pagination
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+if DEBUG:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append(
+        'rest_framework.renderers.BrowsableAPIRenderer')
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
