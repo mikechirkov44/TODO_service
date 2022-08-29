4833870358fadf5f144d517dec6b42d3a9c4d04e
mikechirkov44 <ya.mikechirkov@yandex.ru>
2022-08-29 14:39:40 +0300

    Authentification and logout done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,7 +138,7 @@ AUTH_USER_MODEL = "userapp.User"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -17,7 +17,7 @@ import {
     MDBCardLink,
 } from 'mdb-react-ui-kit';
 
-function MainMenu() {
+function MainMenu(auth) {
     const [showBasic, setShowBasic] = useState(false);
     return (
         <MDBNavbar expand='lg' light bgColor='light'>
@@ -55,11 +55,20 @@ function MainMenu() {
                                 </MDBDropdownMenu>
                             </MDBDropdown>
                         </MDBNavbarItem>
-
-
+                        <MDBNavbarItem>
+                            {auth.is_Auth() ?
+                                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                                    You logged in as,  <b>{auth.currentUser}</b>
+                                </MDBNavbarLink> : <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                                    Anonim user, please Login to udentify yourself
+                                </MDBNavbarLink>}
+                        </MDBNavbarItem>
                     </MDBNavbarNav>
                 </MDBCollapse>
-                <MDBCardLink href='/login'>Login</MDBCardLink>
+                {auth.is_Auth() ?
+                    <MDBNavbarLink href='#' onClick={() => auth.logOut()}>Logout</MDBNavbarLink> :
+                    <MDBNavbarLink href='/login'>Login</MDBNavbarLink>}
+
             </MDBContainer>
         </MDBNavbar>
     );
