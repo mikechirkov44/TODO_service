50cc63e8104d8b4bb269c037b60b1d5ef560f457
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-09-02 08:54:49 +0300

    tests added (#8)

--- backend/userapp/tests.py
+++ backend/userapp/tests.py
@@ -1,1 +1,25 @@
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APIClient
+from mixer.backend.django import mixer
+from .models import User
+from .views import UserModelViewSet
+
+
 # Create your tests here.
+class SmokeUserTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.user = mixer.blend(User)
+
+    def test_get_users_list(self) -> None:
+        factory = APIRequestFactory()
+        request = factory.get('/userapp/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_user_detail(self) -> None:
+        client = APIClient()
+        response = client.get(f'/userapp/api/users/{self.user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
